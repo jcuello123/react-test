5fe537db251fea81499d9e8c114d862ea253b744
jcuello123 <jcuello123@yahoo.com>
2021-11-14 15:23:57 -0500

    initial commit

---
+++ build/precache-manifest.6d25f4692d9a78fecb2a6d6edac459d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19dde58e648afd277d029ad6baccfac2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "761d50fc8422ca515216",
+    "url": "/static/css/main.34330132.chunk.css"
+  },
+  {
+    "revision": "fcdcda5aa5fbbc5d5bce",
+    "url": "/static/js/2.13c4fa53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.13c4fa53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "761d50fc8422ca515216",
+    "url": "/static/js/main.3949bef0.chunk.js"
+  },
+  {
+    "revision": "7e35796a2d538e2a8b66",
+    "url": "/static/js/runtime-main.ea22caeb.js"
+  }
+]);
